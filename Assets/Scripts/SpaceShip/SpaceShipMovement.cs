75a41a5d84ce657bd5e99d280bfc43448c10a29d
Aleksey Moiseenko <2034966@gmail.com>
2020-07-07 19:46:00 +0300

    [ref #3] Add base flight (WIP)

---
+++ Assets/Scripts/SpaceShip/SpaceShipMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace PH.SpaceShip
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class SpaceShipMovement : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] [Min(0)] private float speed;
+
+        private Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            var xMovement = Input.GetAxisRaw("Horizontal");
+            var yMovement = Input.GetAxisRaw("Vertical");
+            
+            var movement = Vector3.ClampMagnitude(new Vector3(xMovement, yMovement, 1f), 1f) * speed * Time.fixedDeltaTime;
+
+            rb.MovePosition(rb.position + movement);
+        }
+    }
+}
