46dd2f553511f454021f6f40319273e22d1a07ee
Aleksey Moiseenko <2034966@gmail.com>
2020-07-21 18:30:08 +0300

    [ref #13] Add healthBar animation

--- Assets/Scripts/UI/Views/HealthBarView.cs
+++ Assets/Scripts/UI/Views/HealthBarView.cs
@@ -6,19 +6,26 @@ using UnityEngine.UI;
 namespace PH.UI.Views
 {
     [RequireComponent(typeof(Image))]
+    [RequireComponent(typeof(Animator))]
     public class HealthBarView : MonoBehaviour, IDamagableTarget
     {
         [SerializeField] private Image healthFillImage;
+        [SerializeField] private float healthLeftToStartPulse;
+
+        Animator animator;
 
         private void Start()
         {
             Messenger.Register<IDamagableTarget>(this);
+            animator = GetComponent<Animator>();
+
             healthFillImage.fillAmount = 1f;
         }
 
         public void SetHealth(float healthPercentage)
         {
             healthFillImage.fillAmount = healthPercentage;
+            animator.SetBool("IsLowHealth", healthPercentage <= healthLeftToStartPulse);
         }
     }
 }
