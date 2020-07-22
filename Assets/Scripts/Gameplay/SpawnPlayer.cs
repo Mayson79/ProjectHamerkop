9e1b36c958d1aa7d30c171235540e3625142188f
Aleksey Moiseenko <2034966@gmail.com>
2020-07-22 19:22:12 +0300

    [ref #15] Add LevelEnd and SpawnPoint

---
+++ Assets/Scripts/Gameplay/SpawnPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace PH.Gameplay
+{
+    public class SpawnPlayer : MonoBehaviour
+    {
+        [SerializeField] private GameObject playerPrefab;
+        private void Start()
+        {
+            Instantiate(playerPrefab, transform.position, transform.rotation);
+        }
+    }
+}
